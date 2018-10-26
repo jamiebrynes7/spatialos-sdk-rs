30f8fc5e2d787801e3199b00fdb93f04a30f9184
Jamie Brynes <jamiebrynes7@gmail.com>
2018-10-26 15:38:17 +0100

    lint build

--- build.rs
+++ build.rs
@@ -9,7 +9,7 @@ use std::process;
 enum SpatialPackageSource {
     WorkerSdk(),
     Tools(),
-    Schema()
+    Schema(),
 }
 
 impl SpatialPackageSource {
@@ -19,15 +19,14 @@ impl SpatialPackageSource {
         match *self {
             WorkerSdk() => "worker_sdk",
             Tools() => "tools",
-            Schema() => "schema"
+            Schema() => "schema",
         }
     }
 }
 
 fn main() {
-    
     let sdk_version = "13.3.0";
-    
+
     // TODO: Maybe this should be done elsewhere?
     /*
     download_and_unpack(SpatialPackageSource::WorkerSdk(), "c-dynamic-x86_64-msvc_mt-win32", sdk_version, "dependencies/win").expect("Could not download package");
@@ -38,88 +37,119 @@ fn main() {
 }
 
 /// Downloads and unpacks a Spatial package into a specified directory.
-/// 
+///
 /// * package_source        - the source of the package
 /// * package_name          - the name of the package
 /// * sdk_version           - the Spatial SDK version
 /// * target_directory      - the target directory to unpack to
-/// 
+///
 /// * returns               - an error if the operation could not be completed, empty otherwise
-fn download_and_unpack(package_source: SpatialPackageSource, package_name: &str, sdk_version: &str, target_directory: &str) -> Result<(), io::Error> {
-    let current_dir = std::env::current_dir()
-        .expect("Could not find current working directory with error");
-    
+fn download_and_unpack(
+    package_source: SpatialPackageSource,
+    package_name: &str,
+    sdk_version: &str,
+    target_directory: &str,
+) -> Result<(), io::Error> {
+    let current_dir =
+        std::env::current_dir().expect("Could not find current working directory with error");
+
     // Clean target directory.
-    // TODO: If things are present, assume they are there and early exit? 
+    // TODO: If things are present, assume they are there and early exit?
     fs::remove_dir_all(target_directory)?;
     fs::create_dir_all(target_directory)?;
-    
+
     // Create temporary directory.
     let mut tmp_dir = current_dir.clone();
     tmp_dir.push("tmp");
     fs::create_dir_all(&tmp_dir)?;
-    
+
     let mut tmp_file = tmp_dir.clone();
     tmp_file.push(package_name);
-    
-    download_package(package_source, package_name, sdk_version, tmp_file.to_str().unwrap());
+
+    download_package(
+        package_source,
+        package_name,
+        sdk_version,
+        tmp_file.to_str().unwrap(),
+    );
     unpack_package(tmp_file.to_str().unwrap(), target_directory)?;
-    
+
     // Clean temp directory.
     fs::remove_dir_all(&tmp_dir)?;
-    
+
     Ok(())
 }
 
 /// Downloads a Spatial package through the spatial CLI.
-/// 
+///
 /// * package_source    - the package source, i.e - worker_sdk, tools, schema.
-fn download_package(package_source: SpatialPackageSource, package_name: &str, sdk_version: &str, target_file: &str) {
-    let args = vec!["package", "retrieve", package_source.to_str(), package_name, sdk_version, target_file];
+fn download_package(
+    package_source: SpatialPackageSource,
+    package_name: &str,
+    sdk_version: &str,
+    target_file: &str,
+) {
+    let args = vec![
+        "package",
+        "retrieve",
+        package_source.to_str(),
+        package_name,
+        sdk_version,
+        target_file,
+    ];
 
     let out = process::Command::new("spatial")
         .args(args)
         .output()
         .expect("Could not run spatial package retrieve");
-    
+
     if !out.status.success() {
         let stdout = match String::from_utf8(out.stdout) {
             Ok(v) => v,
-            Err(e) => panic!("Could not decode stdout from spatial command with error: {}", e)
+            Err(e) => panic!(
+                "Could not decode stdout from spatial command with error: {}",
+                e
+            ),
         };
-        
+
         let stderr = match String::from_utf8(out.stderr) {
             Ok(v) => v,
-            Err(e) => panic!("Could not decode stderr from spatial command with error: {}", e)
+            Err(e) => panic!(
+                "Could not decode stderr from spatial command with error: {}",
+                e
+            ),
         };
-        
-        panic!("spatial package retrieve returned a non-zero error code.\n Stdout: {}\nStderr: {}", stdout, stderr);
+
+        panic!(
+            "spatial package retrieve returned a non-zero error code.\n Stdout: {}\nStderr: {}",
+            stdout, stderr
+        );
     }
 }
 
 /// Unpacks a zip archive into a directory.
-/// 
+///
 /// * target_package_path    - the absolute path of the package archive.
 /// * target_directory       - the absolute path of the target directory.
-/// 
+///
 /// * result                 - an IO error if the operation could not be completed, empty otherwise.
 fn unpack_package(target_package_path: &str, target_directory: &str) -> Result<(), io::Error> {
     // Prepare target directory.
     fs::remove_dir_all(target_directory)?;
     fs::create_dir_all(target_directory)?;
-    
+
     // Unpack zip archive
     let fname = path::Path::new(target_package_path);
     let file = fs::File::open(&fname)?;
     let mut archive = zip::ZipArchive::new(file)?;
-    
+
     for i in 0..archive.len() {
         let mut file = archive.by_index(i)?;
-        
-        let mut output_path =  path::PathBuf::new();
+
+        let mut output_path = path::PathBuf::new();
         output_path.push(target_directory);
         output_path.push(file.sanitized_name());
-        
+
         if (&*file.name()).ends_with('/') {
             fs::create_dir_all(&output_path)?;
         } else {
@@ -128,27 +158,26 @@ fn unpack_package(target_package_path: &str, target_directory: &str) -> Result<(
                     fs::create_dir_all(&p)?;
                 }
             }
-            
+
             let mut outfile = fs::File::create(&output_path)?;
             io::copy(&mut file, &mut outfile)?;
 
             #[cfg(any(unix))]
             {
                 use std::os::unix::fs::PermissionsExt;
-                
+
                 let metadata = outfile.metadata()?;
                 let mut permissions = metadata.permissions();
-                // TODO: Check if is binary before setting executable? 
+                // TODO: Check if is binary before setting executable?
                 permissions.set_mode(0o774);
             }
         }
     }
-    
+
     Ok(())
 }
 
 fn generate_spatial_bindings() {
-    
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -163,5 +192,7 @@ fn generate_spatial_bindings() {
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
