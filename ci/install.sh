aa5199ee0a30e471442f80311e626e40001ee89a
Jamie Brynes <jamiebrynes@improbable.io>
2018-10-31 23:57:22 +0000

    add spatial installer

---
+++ ci/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function isLinux() {
+  [[ "$(uname -s)" == "Linux" ]];
+}
+
+function isMacOS() {
+  [[ "$(uname -s)" == "Darwin" ]];
+}
+
+function isWindows() {
+  ! ( isLinux || isMacOS );
+}
+
+if isWindows; then
+    DOWNLOAD_URL="https://console.improbable.io/toolbelt/download/latest/win"
+elif isMacOS; then
+    DOWNLOAD_URL="https://console.improbable.io/toolbelt/download/latest/mac"
+elif isLinux; then
+    DOWNLOAD_URL="https://console.improbable.io/toolbelt/download/latest/linux"
+else
+    echo "Unknown CI platform."
+    exit 1
+fi
+
+mkdir -p ~/.spatial
+curl -sSLf -o ~/.spatial/spatial "${DOWNLOAD_URL}"
+chmod +x ~/.spatial/spatial
+
+export PATH=${PATH}:~/.spatial/spatial
+
+spatial version
