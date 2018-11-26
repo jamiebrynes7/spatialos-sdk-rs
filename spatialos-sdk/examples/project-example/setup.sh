5147efa2d74fcd86e5540f87a76a8c351b042dd2
Jamie Brynes <jamiebrynes7@gmail.com>
2018-11-26 23:42:32 +0000

    Fix Setup Script (#28)
    Title

--- spatialos-sdk/examples/project-example/setup.sh
+++ spatialos-sdk/examples/project-example/setup.sh
@@ -8,11 +8,11 @@ DEPENDENCIES="../../../dependencies"
 SCHEMA_COMPILER="${DEPENDENCIES}/schema-compiler/schema_compiler"
 PROTOC="${DEPENDENCIES}/schema-compiler/protoc"
 
-mkdir -p "spatial-os/schema/bin"
+mkdir -p "spatialos/schema/bin"
 mkdir -p "tmp"
 
 cp -r ${DEPENDENCIES}/schema-compiler/proto/* "./tmp"
 "${SCHEMA_COMPILER}" --schema_path="${DEPENDENCIES}/std-lib" --proto_out="./tmp" --load_all_schema_on_schema_path ${DEPENDENCIES}/std-lib/improbable/*.schema
-"${PROTOC}" --proto_path="./tmp" --descriptor_set_out="./spatial-os/schema/bin/schema.descriptor" --include_imports  tmp/**/*.proto
+"${PROTOC}" --proto_path="./tmp" --descriptor_set_out="./spatialos/schema/bin/schema.descriptor" --include_imports  tmp/**/*.proto
 
 rm -rf "tmp"
