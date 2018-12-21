f330f659448120a9215f350963cbd797cdb47fc8
Jamie Brynes <jamiebrynes@improbable.io>
2018-12-21 10:17:56 -0800

    add worker type argument

--- spatialos-sdk/examples/project-example/lib/argument_parsing.rs
+++ spatialos-sdk/examples/project-example/lib/argument_parsing.rs
@@ -1,13 +1,12 @@
 extern crate clap;
 
-use self::clap::{App, Arg, SubCommand};
+use self::clap::{App, Arg, ArgMatches, SubCommand};
 
 use spatialos_sdk::worker::locator::{LocatorCredentials, LocatorParameters};
 use spatialos_sdk::worker::parameters::{ConnectionParameters, ProtocolLoggingParameters};
 
 static RECEPTIONIST_HOST: &str = "127.0.0.1";
 static RECEPTIONIST_PORT: u16 = 7777;
-static WORKER_TYPE: &str = "RustWorker";
 
 static RECEPTIONIST_SUBCOMMAND: &str = "receptionist";
 static LOCATOR_SUBCOMMAND: &str = "locator";
@@ -16,6 +15,7 @@ static CONNECT_POLL_ARG: &str = "connect_with_poll";
 static EXTERNAL_IP_ARG: &str = "use_external_ip";
 static LOCATOR_TOKEN_ARG: &str = "locator_token";
 static PROJECT_NAME_ARG: &str = "project_name";
+static WORKER_TYPE_ARG: &str = "worker_type";
 
 pub enum ConnectionType {
     Receptionist(String, u16),
@@ -35,6 +35,13 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
         .short("p")
         .help("Uses the polling connect rather than the blocking connect.");
 
+    let worker_type_arg = Arg::with_name(WORKER_TYPE_ARG)
+        .long(WORKER_TYPE_ARG)
+        .short("w")
+        .value_name("WORKER_TYPE")
+        .required(true)
+        .help("The worker type to use when connecting. Either: RustClient or RustServer.");
+
     let matches = App::new("SpatialOS Rust SDK Example Worker")
         .author("Jamie Brynes <jamiebrynes7@gmail.com>")
         .about("An example usage of the SpatialOS Rust SDK.")
@@ -42,6 +49,7 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
             SubCommand::with_name(RECEPTIONIST_SUBCOMMAND)
                 .about("Connect via receptionist.")
                 .arg(&polling_connection_arg)
+                .arg(&worker_type_arg)
                 .arg(
                     Arg::with_name(EXTERNAL_IP_ARG)
                         .long(EXTERNAL_IP_ARG)
@@ -52,6 +60,7 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
             SubCommand::with_name(LOCATOR_SUBCOMMAND)
                 .about("Connect via locator.")
                 .arg(&polling_connection_arg)
+                .arg(&worker_type_arg)
                 .arg(
                     Arg::with_name(LOCATOR_TOKEN_ARG)
                         .long(LOCATOR_TOKEN_ARG)
@@ -70,22 +79,25 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
                 ),
         ).get_matches();
 
-    let mut params = ConnectionParameters::new(WORKER_TYPE).using_tcp();
+    let mut params = ConnectionParameters::new("").using_tcp();
 
     if let Some(sub_matches) = matches.subcommand_matches(LOCATOR_SUBCOMMAND) {
         let locator_params = LocatorParameters::new(
             sub_matches
                 .value_of(PROJECT_NAME_ARG)
-                .expect("WHAT")
+                .expect("No project name found.")
                 .to_string(),
             LocatorCredentials::LoginToken(
                 sub_matches
                     .value_of(LOCATOR_TOKEN_ARG)
-                    .expect("WHAT")
+                    .expect("No locator token found.")
                     .to_string(),
             ),
         );
 
+        let worker_type= get_worker_type(sub_matches);
+
+        params.worker_type = worker_type.to_owned();
         params = params.using_external_ip();
 
         return WorkerConfiguration {
@@ -100,6 +112,9 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
             params = params.using_external_ip();
         }
 
+        let worker_type= get_worker_type(sub_matches);
+        params.worker_type = worker_type.to_owned();
+
         return WorkerConfiguration {
             connection_params: params,
             connect_with_poll: sub_matches.is_present(CONNECT_POLL_ARG),
@@ -111,4 +126,18 @@ pub fn get_worker_configuration() -> WorkerConfiguration {
     }
 
     panic!("Please select one of the subcommands.")
+}
+
+fn get_worker_type<'a>(matches: &'a ArgMatches) -> &'a str {
+    let worker_type = matches
+        .value_of(WORKER_TYPE_ARG)
+        .expect("No worker type found.");
+
+    match worker_type {
+        "RustClient" => {},
+        "RustWorker" => {},
+        _ => panic!("Unknown worker type: {}", worker_type)
+    };
+
+    worker_type
 }
