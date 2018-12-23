ac60f4bb14a766ce0190b649dd9afffd11bcce49
Jamie Brynes <jamiebrynes@improbable.io>
2018-12-23 07:29:26 -0800

    add clippy to lint step

--- ci/lint.sh
+++ ci/lint.sh
@@ -5,6 +5,16 @@ if [[ -n "${DEBUG-}" ]]; then
   set -x
 fi
 
+cd "$(dirname $0)/../"
+
 cargo fmt -- --check
+
+pushd spatialos-sdk
+    cargo clippy --all-targets --all-features -- -D warnings -A dead-code
+popd
+
+pushd spatialos-sdk-tools
+    cargo clippy --all-targets --all-features -- -D warnings -A dead-code
+popd
 
 exit 0
