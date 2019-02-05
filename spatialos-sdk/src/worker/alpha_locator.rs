85baaf23ae1e398bd6bba9f655ed8f1b09ab2b75
Jamie Brynes <jamiebrynes7@gmail.com>
2019-02-05 22:00:33 +0000

    implement login tokens + lint

--- spatialos-sdk/src/worker/alpha_locator.rs
+++ spatialos-sdk/src/worker/alpha_locator.rs
@@ -4,10 +4,8 @@ use futures::{Async, Future};
 
 use spatialos_sdk_sys::worker::*;
 
-use crate::worker:: {
-    ConnectionStatusCode,
-    parameters::ProtocolLoggingParameters ,
-    internal::utils::cstr_to_string
+use crate::worker::{
+    internal::utils::cstr_to_string, parameters::ProtocolLoggingParameters, ConnectionStatusCode,
 };
 
 pub struct AlphaLocator {}
@@ -21,7 +19,11 @@ impl AlphaLocator {
         unsafe {
             let cstr = CString::new(hostname).unwrap();
             let (mut params, data) = params.to_worker_sdk();
-            PlayerIdentityTokenFuture::new(Worker_Alpha_CreateDevelopmentPlayerIdentityTokenAsync(cstr.as_ptr(), port, &mut params as *mut _))
+            PlayerIdentityTokenFuture::new(Worker_Alpha_CreateDevelopmentPlayerIdentityTokenAsync(
+                cstr.as_ptr(),
+                port,
+                &mut params as *mut _,
+            ))
         }
     }
 }
@@ -101,7 +103,7 @@ impl PlayerIdentityTokenRequest {
                 player_id: underlying_data[1].as_ptr(),
                 duration_seconds: match self.duration_seconds {
                     Some(ref mut value) => value as *mut u32,
-                    None => ::std::ptr::null_mut()
+                    None => ::std::ptr::null_mut(),
                 },
                 display_name: match self.display_name {
                     Some(_) => underlying_data[2].as_ptr(),
@@ -126,7 +128,7 @@ pub struct PlayerIdentityTokenResponse {
 impl PlayerIdentityTokenResponse {
     fn from_worker_sdk(response: &Worker_Alpha_PlayerIdentityTokenResponse) -> Self {
         PlayerIdentityTokenResponse {
-            player_identity_token: cstr_to_string(response.player_identity_token)
+            player_identity_token: cstr_to_string(response.player_identity_token),
         }
     }
 }
@@ -151,7 +153,8 @@ impl PlayerIdentityTokenFuture {
         assert!(!response.is_null());
         unsafe {
             let response = *response;
-            let data = &mut *(user_data as *mut Option<Result<PlayerIdentityTokenResponse, String>>);
+            let data =
+                &mut *(user_data as *mut Option<Result<PlayerIdentityTokenResponse, String>>);
             if !response.error.is_null() {
                 let err = cstr_to_string(response.error);
                 *data = Some(Err(err));
@@ -161,6 +164,25 @@ impl PlayerIdentityTokenFuture {
             let response = PlayerIdentityTokenResponse::from_worker_sdk(&response);
             *data = Some(Ok(response));
         }
+    }
+
+    fn get_future(
+        &self,
+        timeout: Option<u32>,
+    ) -> Option<Result<PlayerIdentityTokenResponse, String>> {
+        assert!(!self.internal.is_null());
+
+        let mut data: Option<Result<PlayerIdentityTokenResponse, String>> = None;
+        unsafe {
+            Worker_Alpha_PlayerIdentityTokenResponseFuture_Get(
+                self.internal,
+                timeout.map_or(::std::ptr::null(), |value| &value),
+                &mut data as *mut _ as *mut ::std::os::raw::c_void,
+                Some(PlayerIdentityTokenFuture::callback_handler),
+            );
+        }
+
+        data
     }
 }
 
@@ -173,21 +195,11 @@ impl Future for PlayerIdentityTokenFuture {
             return Err("PlayerIdentityTokenFuture has already been consumed".to_owned());
         }
 
-        assert!(!self.internal.is_null());
-        let mut data: Option<Result<PlayerIdentityTokenResponse, String>> = None;
-        unsafe {
-            Worker_Alpha_PlayerIdentityTokenResponseFuture_Get(
-                self.internal,
-                &0,
-                &mut data as *mut _ as *mut ::std::os::raw::c_void,
-                Some(PlayerIdentityTokenFuture::callback_handler),
-            );
-        }
-
-        data.map_or(Ok(Async::NotReady), |result| {
-            self.consumed = true;
-            result.map(|resp| Async::Ready(resp))
-        })
+        self.get_future(Some(0))
+            .map_or(Ok(Async::NotReady), |result| {
+                self.consumed = true;
+                result.map(|resp| Async::Ready(resp))
+            })
     }
 
     fn wait(self) -> Result<<Self as Future>::Item, <Self as Future>::Error>
@@ -198,18 +210,7 @@ impl Future for PlayerIdentityTokenFuture {
             return Err("PlayerIdentityTokenFuture has already been consumed".to_owned());
         }
 
-        assert!(!self.internal.is_null());
-        let mut data: Option<Result<PlayerIdentityTokenResponse, String>> = None;
-        unsafe {
-            Worker_Alpha_PlayerIdentityTokenResponseFuture_Get(
-                self.internal,
-                ::std::ptr::null(),
-                &mut data as *mut _ as *mut ::std::os::raw::c_void,
-                Some(PlayerIdentityTokenFuture::callback_handler),
-            );
-        }
-
-        data.expect("Blocking call to Worker_Alpha_PlayerIdentityTokenResponseFuture_Get did not trigger callback")
+        self.get_future(None).expect("Blocking call to Worker_Alpha_PlayerIdentityTokenResponseFuture_Get did not trigger callback")
     }
 }
 
@@ -217,6 +218,188 @@ impl Drop for PlayerIdentityTokenFuture {
     fn drop(&mut self) {
         if !self.internal.is_null() {
             unsafe { Worker_Alpha_PlayerIdentityTokenResponseFuture_Destroy(self.internal) }
+        }
+    }
+}
+
+pub struct LoginTokensRequest {
+    pub player_identity_token: String,
+    pub worker_type: String,
+    pub duration_seconds: Option<u32>,
+    pub use_insecure_connection: bool,
+}
+
+impl LoginTokensRequest {
+    pub fn new<S: Into<String>, T: Into<String>>(player_identity_token: S, worker_type: T) -> Self {
+        LoginTokensRequest {
+            player_identity_token: player_identity_token.into(),
+            worker_type: worker_type.into(),
+            duration_seconds: None,
+            use_insecure_connection: false,
+        }
+    }
+
+    pub fn with_duration_seconds(mut self, duration: u32) -> Self {
+        self.duration_seconds = Some(duration);
+        self
+    }
+
+    pub fn with_insecure_connection(mut self) -> Self {
+        self.use_insecure_connection = true;
+        self
+    }
+
+    fn to_worker_sdk(&mut self) -> (Worker_Alpha_LoginTokensRequest, Vec<CString>) {
+        unsafe {
+            let underlying_data = vec![
+                CString::new(self.player_identity_token.as_str()).unwrap(),
+                CString::new(self.worker_type.as_str()).unwrap(),
+            ];
+
+            let request = Worker_Alpha_LoginTokensRequest {
+                player_identity_token: underlying_data[0].as_ptr(),
+                worker_type: underlying_data[1].as_ptr(),
+                duration_seconds: match self.duration_seconds {
+                    Some(ref mut value) => value as *mut u32,
+                    None => ::std::ptr::null_mut(),
+                },
+                use_insecure_connection: self.use_insecure_connection as u8,
+            };
+
+            (request, underlying_data)
+        }
+    }
+}
+
+pub struct LoginTokensResponse {
+    pub login_tokens: Vec<LoginTokenDetails>,
+}
+
+impl LoginTokensResponse {
+    fn from_worker_sdk(response: &Worker_Alpha_LoginTokensResponse) -> Self {
+        unsafe {
+            let tokens = ::std::slice::from_raw_parts(
+                response.login_tokens,
+                response.login_token_count as usize,
+            )
+            .iter()
+            .map(|token| LoginTokenDetails::from_worker_sdk(token))
+            .collect::<Vec<LoginTokenDetails>>();
+
+            LoginTokensResponse {
+                login_tokens: tokens,
+            }
+        }
+    }
+}
+
+pub struct LoginTokenDetails {
+    pub deployment_id: String,
+    pub deployment_name: String,
+    pub tags: Vec<String>,
+    pub login_token: String,
+}
+
+impl LoginTokenDetails {
+    fn from_worker_sdk(token_details: &Worker_Alpha_LoginTokenDetails) -> Self {
+        unsafe {
+            let tags =
+                ::std::slice::from_raw_parts(token_details.tags, token_details.tag_count as usize)
+                    .iter()
+                    .map(|tag| cstr_to_string(*tag))
+                    .collect::<Vec<String>>();
+
+            LoginTokenDetails {
+                deployment_id: cstr_to_string(token_details.deployment_id),
+                deployment_name: cstr_to_string(token_details.deployment_name),
+                tags,
+                login_token: cstr_to_string(token_details.login_token),
+            }
+        }
+    }
+}
+
+pub struct LoginTokensFuture {
+    internal: *mut Worker_Alpha_LoginTokensResponseFuture,
+    consumed: bool,
+}
+
+impl LoginTokensFuture {
+    fn new(ptr: *mut Worker_Alpha_LoginTokensResponseFuture) -> Self {
+        LoginTokensFuture {
+            internal: ptr,
+            consumed: false,
+        }
+    }
+
+    extern "C" fn callback_handler(
+        user_data: *mut ::std::os::raw::c_void,
+        response: *const Worker_Alpha_LoginTokensResponse,
+    ) {
+        assert!(!response.is_null());
+        unsafe {
+            let response = *response;
+            let data = &mut *(user_data as *mut Option<Result<LoginTokensResponse, String>>);
+            if !response.error.is_null() {
+                let err = cstr_to_string(response.error);
+                *data = Some(Err(err));
+                return;
+            }
+
+            let response = LoginTokensResponse::from_worker_sdk(&response);
+            *data = Some(Ok(response));
+        }
+    }
+
+    fn get_future(&self, timeout: Option<u32>) -> Option<Result<LoginTokensResponse, String>> {
+        assert!(!self.internal.is_null());
+        let mut data: Option<Result<LoginTokensResponse, String>> = None;
+        unsafe {
+            Worker_Alpha_LoginTokensResponseFuture_Get(
+                self.internal,
+                timeout.map_or(::std::ptr::null(), |value| &value),
+                &mut data as *mut _ as *mut ::std::os::raw::c_void,
+                Some(LoginTokensFuture::callback_handler),
+            );
+
+            data
+        }
+    }
+}
+
+impl Future for LoginTokensFuture {
+    type Item = LoginTokensResponse;
+    type Error = String;
+
+    fn poll(&mut self) -> Result<Async<<Self as Future>::Item>, <Self as Future>::Error> {
+        if self.consumed {
+            return Err("LoginTokensFuture has already been consumed".to_owned());
+        }
+
+        self.get_future(Some(0))
+            .map_or(Ok(Async::NotReady), |result| {
+                self.consumed = true;
+                result.map(|resp| Async::Ready(resp))
+            })
+    }
+
+    fn wait(self) -> Result<<Self as Future>::Item, <Self as Future>::Error>
+    where
+        Self: Sized,
+    {
+        if self.consumed {
+            return Err("LoginTokensFuture has already been consumed".to_owned());
+        }
+
+        self.get_future(None)
+            .expect("Blocking call to Worker_Alpha_LoginTokensFuture_Get did not trigger callback")
+    }
+}
+
+impl Drop for LoginTokensFuture {
+    fn drop(&mut self) {
+        if !self.internal.is_null() {
+            unsafe { Worker_Alpha_LoginTokensResponseFuture_Destroy(self.internal) }
         }
     }
 }
