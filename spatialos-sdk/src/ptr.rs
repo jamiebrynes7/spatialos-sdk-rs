05a13b37ae8af19cfcc818d492ecc9054d8981c1
David LeGare <dlegare.1001@gmail.com>
2019-02-07 21:08:06 -0600

    Update documentation for MutPtr

--- spatialos-sdk/src/ptr.rs
+++ spatialos-sdk/src/ptr.rs
@@ -1,13 +1,24 @@
 //! Utilities for handling raw pointers.
 
 /// Wrapper around a raw pointer that ensures the pointer can only be accessed
-/// through `&mut self`.
+/// through a mutable reference.
+///
+/// Some types in the C API can be safely shared between threads as long as they are
+/// only accessed by one thread at a time. We can enforce this invariant in Rust
+/// without a `Mutex` by requiring that the data be accessed only through a mutable
+/// reference. However, a `*mut T` doesn't follow Rust's usual ownership rules, and
+/// can still be used through a shared reference.
+///
+/// `MutPtr` wraps wraps a raw pointer and only allows the raw value to be accessed
+/// through a mutable reference, statically ensuring that the thread-safety
+/// requirements aren't violated.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct MutPtr<T> {
     ptr: *mut T,
 }
 
 impl<T> MutPtr<T> {
+    /// Creates a new `MutPtr` wrapping `ptr`.
     pub fn new(ptr: *mut T) -> Self {
         MutPtr { ptr }
     }
