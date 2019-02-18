4f09bd8bee412839b14ae97907f0186732595c0c
David LeGare <dlegare.1001@gmail.com>
2019-02-18 10:27:41 -0600

    Perform cleanup in project-example

--- project-example/src/main.rs
+++ project-example/src/main.rs
@@ -49,9 +49,21 @@ fn main() {
 }
 
 fn logic_loop(c: &mut WorkerConnection) {
-    let mut world = HashMap::new();
+    /// Local tracking of the state of an entity's components. We only track the
+    /// `Rotate` component because it's the only one we care about for this demo.
+    #[derive(Debug, Default)]
+    struct EntityState {
+        has_authority: bool,
+        rotate: Option<example::Rotate>,
+    }
 
     let mut rng = rand::thread_rng();
+
+    // Store the currently-visible state of the world. Entities/components are added
+    // and removed from the world as we get ops notifying us of those changes. The
+    // data in `world` also tracks which `Rotate` components we currently have
+    // authority over, so that we know which ones we need to be updating.
+    let mut world = HashMap::new();
 
     // Create an entity with a `Rotate` component in order to demonstrate tracking and
     // updating entities.
@@ -110,14 +122,26 @@ fn logic_loop(c: &mut WorkerConnection) {
             }
 
             match op {
+                // When an entity first enters the area of interest for the worker, we add
+                // it to our local tracking with the default (i.e. empty) state. As we
+                // receive further ops, we will update the component state and authority for
+                // the entity.
                 WorkerOp::AddEntity(add_entity_op) => {
                     world.insert(add_entity_op.entity_id, EntityState::default());
                 }
 
+                // Once an entity leaves our area of interest, we remove it from our local
+                // world view.
                 WorkerOp::RemoveEntity(remove_entity_op) => {
                     world.remove(&remove_entity_op.entity_id);
                 }
 
+                // Add local tracking for a given component. We only track the `Rotate`
+                // component for the purpose of this example.
+                //
+                // NOTE: This assumes that the entity is already present in `world`. This
+                // is a safe assumption to make because SpatialOS will always notify us that
+                // of a new entity before sending any component data for that entity.
                 WorkerOp::AddComponent(add_component) => match add_component.component_id {
                     example::Rotate::ID => {
                         let rotate = add_component.get::<example::Rotate>().unwrap();
@@ -129,6 +153,9 @@ fn logic_loop(c: &mut WorkerConnection) {
                     id => println!("Received unknown component: {}", id),
                 },
 
+                // Track authority changes to the `Rotate` component. We only want to update
+                // entities where we're authoritative over the `Rotate` component, so we
+                // need to know which entities have authoritative over.
                 WorkerOp::AuthorityChange(authority_change) => {
                     if authority_change.component_id == example::Rotate::ID {
                         println!(
@@ -141,6 +168,10 @@ fn logic_loop(c: &mut WorkerConnection) {
                     }
                 }
 
+                // Update the locally tracked state of a component. We override any changes
+                // that we have made locally with the data sent by the server in order to
+                // ensure that we're always working off the latest canonical state of the
+                // component.
                 WorkerOp::ComponentUpdate(update) => match update.component_id {
                     example::Rotate::ID => {
                         let component_update = update.get::<example::Rotate>().unwrap();
@@ -155,17 +186,24 @@ fn logic_loop(c: &mut WorkerConnection) {
             }
         }
 
-        // Update the rotation of all rotation components that we have control over.
+        // Perform update logic for all entities that we have authority over. Note that
+        // we only want to update entities that:
+        //
+        // * Are in our area of intersted (i.e. are represented in `world`).
+        // * Have a `Rotate` component.
+        // * We have authority over the `Rotate` component.
         for (&entity_id, entity_state) in &mut world {
-            // Ignore any entities where we don't have authority over the `Rotate` component.
             if !entity_state.has_authority {
                 continue;
             }
 
             // Only update entities that have a `Rotate` component.
             if let Some(rotate) = &mut entity_state.rotate {
+                // Update the local angle of the `Rotate` component.
                 rotate.angle += f64::consts::PI * 2.0 / 200.0;
 
+                // Send an update to SpatialOS to apply the same update to the official component
+                // state.
                 c.send_component_update::<example::Rotate>(
                     entity_id,
                     example::RotateUpdate {
@@ -175,6 +213,8 @@ fn logic_loop(c: &mut WorkerConnection) {
                     UpdateParameters { loopback: true },
                 );
 
+                // Update the entity's position based on the current state of the `Rotate`
+                // component.
                 c.send_component_update::<improbable::Position>(
                     entity_id,
                     improbable::PositionUpdate {
@@ -189,6 +229,7 @@ fn logic_loop(c: &mut WorkerConnection) {
             }
         }
 
+        // Run the main loop at approximately 30 fps.
         ::std::thread::sleep(::std::time::Duration::from_millis(30));
     }
 }
@@ -200,7 +241,6 @@ fn exercise_connection_code_paths(c: &mut WorkerConnection) {
 
     let _ = c.get_op_list(0);
     c.send_reserve_entity_ids_request(ReserveEntityIdsRequest(1), None);
-    //c.send_delete_entity_request(DeleteEntityRequest(EntityId::new(1)), None);
     send_query(c);
 
     let interested = vec![
@@ -267,10 +307,4 @@ fn send_metrics(c: &mut WorkerConnection) {
     histogram_metric.add_sample(0.5);
 
     c.send_metrics(&m);
-}
-
-#[derive(Debug, Default)]
-struct EntityState {
-    has_authority: bool,
-    rotate: Option<example::Rotate>,
 }
