07684c8435b256808ab2b993644db2a6e8ad9d1f
Jamie Brynes <jamiebrynes7@gmail.com>
2019-12-20 13:54:51 +0000

    use option

--- spatialos-sdk/src/worker/worker_future.rs
+++ spatialos-sdk/src/worker/worker_future.rs
@@ -8,13 +8,13 @@ use std::{
 };
 
 pub struct WorkerFuture<T: WorkerSdkFuture> {
-    inner: WorkerFutureState<T>,
+    inner: Option<WorkerFutureState<T>>,
 }
 
 impl<T: WorkerSdkFuture> WorkerFuture<T> {
     pub fn new(future: T) -> Self {
         WorkerFuture {
-            inner: WorkerFutureState::NotStarted(future),
+            inner: Some(WorkerFutureState::NotStarted(future)),
         }
     }
 }
@@ -31,8 +31,10 @@ struct InProgressHandle<T: WorkerSdkFuture> {
 
 impl<T: WorkerSdkFuture> Drop for WorkerFuture<T> {
     fn drop(&mut self) {
-        if let WorkerFutureState::InProgress(handle) = &mut self.inner {
-            handle.thread.take().unwrap().join().unwrap();
+        if let Some(future_state) = &mut self.inner {
+            if let WorkerFutureState::InProgress(handle) = future_state {
+                handle.thread.take().unwrap().join().unwrap();
+            }
         }
     }
 }
@@ -50,49 +52,38 @@ impl<T: WorkerSdkFuture + 'static> Future for WorkerFuture<T> {
     type Output = T::Output;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let future_state = &mut self.as_mut().get_mut().inner;
+        let inner = &mut self.as_mut().get_mut().inner;
+        // This panics if the future is polled after completion. This is acceptable according to
+        // the Future trait contract - https://doc.rust-lang.org/std/future/trait.Future.html#panics
+        let mut future_state = inner.take().unwrap();
 
         match future_state {
-            // We don't want to borrow the WorkerSdkFuture object. We want to take it so we can
-            // send it to the thread which will block on the Worker SDK future.
-            // To do this, we construct the next state and then mem swap them.
-            // We do have to add the thread handle _after_ we start the thread though,
-            // so it gets a little awkward.
-            // Rust's enum destructing lets us down a little bit here.
-            WorkerFutureState::NotStarted(_) => {
+            WorkerFutureState::NotStarted(ftr) => {
                 let (tx, rx) = futures::channel::oneshot::channel();
                 let task = cx.waker().clone();
 
-                let mut state = WorkerFutureState::InProgress(InProgressHandle::<T> {
-                    result_rx: rx,
-                    thread: None,
+                let thread = thread::spawn(move || unsafe {
+                    let ptr = ftr.start();
+                    let value = T::get(ptr);
+                    tx.send(value).unwrap_or(());
+                    task.wake();
+                    T::destroy(ptr)
                 });
 
-                std::mem::swap(future_state, &mut state);
+                let handle = InProgressHandle::<T> {
+                    result_rx: rx,
+                    thread: Some(thread),
+                };
 
-                // This is always true.
-                if let WorkerFutureState::NotStarted(ftr) = state {
-                    let thread = thread::spawn(move || unsafe {
-                        let ptr = ftr.start();
-                        let value = T::get(ptr);
-                        tx.send(value).unwrap_or(());
-                        task.wake();
-                        T::destroy(ptr)
-                    });
-
-                    // As is this one.
-                    if let WorkerFutureState::InProgress(handle) = future_state {
-                        handle.thread = Some(thread);
-                    }
-                }
-
+                *inner = Some(WorkerFutureState::InProgress(handle));
                 Poll::Pending
             }
-            WorkerFutureState::InProgress(handle) => {
+            WorkerFutureState::InProgress(ref mut handle) => {
                 if let Some(val) = handle.result_rx.try_recv().unwrap_or(None) {
                     return Poll::Ready(val);
                 }
 
+                *inner = Some(future_state);
                 Poll::Pending
             }
         }
